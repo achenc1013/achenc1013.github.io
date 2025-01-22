9d959edf812a7d40f172c7b079dba473b385a440
achenc1013 <1013199991@qq.com>
2025-01-22 17:00:36 +0800

    更改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,10 +3,10 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/lucifrix_blog/",
+  base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
+  title: "博客",
   description: "vuepress-theme-hope 的博客演示",
 
   theme,
