6348fb8af43f383d4ad901dea50313b24dbb2537
achenc1013 <1013199991@qq.com>
2025-01-24 20:51:33 +0800

    修复

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,6 @@ import { defineUserConfig } from "vuepress";
 
 import theme from "./theme.js";
 
-import dynamicTitle from "vuepress-plugin-dynamic-title";
 
 export default defineUserConfig({
   base: "/",
@@ -13,15 +12,7 @@ export default defineUserConfig({
 
   theme,
   
-  plugins: [
-    dynamicTitle({
-      showIcon: "/favicon.ico",
-      showText: "(/≧▽≦/)欢迎回来！",
-      hideIcon: "/favicon.ico",
-      hideText: "(●—●)不要走开！",
-      recoverTime: 2000,
-    }),
-  ],
+
  
   // 和 PWA 一起启用
   // shouldPrefetch: false,
